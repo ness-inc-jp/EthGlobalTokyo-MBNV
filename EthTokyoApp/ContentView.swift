82a5c7f808765b4e9c2e779f7e533915e652ab89
0xsuimmy <smny0714@gmail.com>
2023-04-15 12:32:47 +0900

    Initial Commit

---
+++ EthTokyoApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  EthTokyoApp
+//
+//  Created by 住居尚弥 on 2023/04/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EthTokyoApp/EthTokyoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EthTokyoAppApp.swift
+//  EthTokyoApp
+//
+//  Created by 住居尚弥 on 2023/04/15.
+//
+
+import SwiftUI
+
+@main
+struct EthTokyoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
